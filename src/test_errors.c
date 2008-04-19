f94f4a86fa6476600567e3816364ac5ccebf0aa4
phf <phf@77a3a24b-2c49-0410-90f5-8a0d088e77bf>
2008-04-19 21:15:34 +0000

    Formatting was wrong, wow... :-/
    git-svn-id: svn://svn.code.sf.net/p/dasm-dillon/code/trunk@176 77a3a24b-2c49-0410-90f5-8a0d088e77bf

--- src/test_errors.c
+++ src/test_errors.c
@@ -40,31 +40,31 @@ INCFILE *pIncfile = NULL;
 
 int main(void)
 {
-  /* fake a current file */
-  pIncfile = malloc(sizeof(INCFILE));
-  pIncfile->next = NULL;
-  pIncfile->name = "someFileName";
-  pIncfile->lineno = 47;
-  /* enable all messages */
-  F_error_level = ERRORLEVEL_DEBUG;
-  /* test SUPER new API :-) */
-  debug_fmt(MESSAGE_RANGE, "CRAZY", "DEBUG", 2, 14);
-  error_fmt(MESSAGE_RANGE, "CRAZY", NULL, 2, 14);
-  fatal_fmt(MESSAGE_RANGE, "CRAZY", "FATAL", 2, 14);
-  panic_fmt(MESSAGE_RANGE, "CRAZY", NULL, 2, 14);
-  /* test new API */
-  notify(ERROR_PROCESSOR_NOT_SUPPORTED, ERRORLEVEL_ERROR, "new API notify()");
-  debug(ERROR_PROCESSOR_NOT_SUPPORTED, "new API debug()");
-  info(ERROR_PROCESSOR_NOT_SUPPORTED, "new API info()");
-  notice(ERROR_PROCESSOR_NOT_SUPPORTED, "new API notice()");
-  warning(ERROR_PROCESSOR_NOT_SUPPORTED, "new API warning()");
-  error(ERROR_PROCESSOR_NOT_SUPPORTED, "new API error()");
-  fatal(ERROR_PROCESSOR_NOT_SUPPORTED, "new API fatal()");
-  new_panic(ERROR_PROCESSOR_NOT_SUPPORTED, "new API panic()");
-  /* test wrappers for old API */
-  panic("old API panic()");
-  asmerr(ERROR_PROCESSOR_NOT_SUPPORTED, true, "old API asmerr()");
-  return EXIT_SUCCESS;
+    /* fake a current file */
+    pIncfile = malloc(sizeof(INCFILE));
+    pIncfile->next = NULL;
+    pIncfile->name = "someFileName";
+    pIncfile->lineno = 47;
+    /* enable all messages */
+    F_error_level = ERRORLEVEL_DEBUG;
+    /* test SUPER new API :-) */
+    debug_fmt(MESSAGE_RANGE, "CRAZY", "DEBUG", 2, 14);
+    error_fmt(MESSAGE_RANGE, "CRAZY", NULL, 2, 14);
+    fatal_fmt(MESSAGE_RANGE, "CRAZY", "FATAL", 2, 14);
+    panic_fmt(MESSAGE_RANGE, "CRAZY", NULL, 2, 14);
+    /* test new API */
+    notify(ERROR_PROCESSOR_NOT_SUPPORTED, ERRORLEVEL_ERROR, "new API notify()");
+    debug(ERROR_PROCESSOR_NOT_SUPPORTED, "new API debug()");
+      info(ERROR_PROCESSOR_NOT_SUPPORTED, "new API info()");
+    notice(ERROR_PROCESSOR_NOT_SUPPORTED, "new API notice()");
+    warning(ERROR_PROCESSOR_NOT_SUPPORTED, "new API warning()");
+    error(ERROR_PROCESSOR_NOT_SUPPORTED, "new API error()");
+    fatal(ERROR_PROCESSOR_NOT_SUPPORTED, "new API fatal()");
+    new_panic(ERROR_PROCESSOR_NOT_SUPPORTED, "new API panic()");
+    /* test wrappers for old API */
+    panic("old API panic()");
+    asmerr(ERROR_PROCESSOR_NOT_SUPPORTED, true, "old API asmerr()");
+    return EXIT_SUCCESS;
 }
 
 /* vim: set tabstop=4 softtabstop=4 expandtab shiftwidth=4 autoindent: */

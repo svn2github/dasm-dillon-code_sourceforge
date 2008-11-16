a15e9ab9071110a68db35875703d1401e783e8b1
phf <phf@77a3a24b-2c49-0410-90f5-8a0d088e77bf>
2008-11-16 10:22:06 +0000

    Finished with supercat bug. I hope. :-)
    git-svn-id: svn://svn.code.sf.net/p/dasm-dillon/code/trunk@322 77a3a24b-2c49-0410-90f5-8a0d088e77bf

--- test/supercat.asm
+++ test/supercat.asm
@@ -1,6 +1,10 @@
 ; $Id$
 ;
-; Pretty horrible bug... :-/
+; Pretty horrible bug... :-/ Switching to signed instead
+; of unsigned arithmetic made this easier to handle but
+; bad use of .ds can still lead to huge files since DASM
+; doesn't enforce an upper bound yet. And what should it
+; be? Hmm...
 ;
 ; From "supercat" on the atariage.com forums, forwarded by
 ; Andrew Davie 2008/04/12.
@@ -10,18 +14,19 @@
 
 	.processor 6502
 
-	org	$7FF8
-	byte	1,2,3,4,5
+	.org	$7FF8
+	.byte	1,2,3,4,5
 
-; original line by "supercat", disabled to avoid producing
-; huge files by accident...
-;	ds	$7FFC-*
+; original line by "supercat", would produce huge files
+; since the "-1" result would be interpreted as a huge
+; positive number
+	.ds	$7FFC-*
 
-; Peter's line making the problem tractable: a negative number
-; is taken as a huge positive one during expression evaluation,
-; so "ds" above tries to produce a huge object file... :-/
-	long	$7FFC-*
+; Peter's line made the problem tractable and revealed
+; the actual bug, checking this with xxd clearly shows
+; the -1 bit pattern 0xffffffff which unsigned is huge.
+	.long	$7FFC-*
 
-	byte	1,2,3,4
+	.byte	1,2,3,4,5
 
 	.end

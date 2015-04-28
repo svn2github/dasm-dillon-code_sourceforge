b2911d8b268e00f104b2f55320066be94ffdf969
phf <phf@77a3a24b-2c49-0410-90f5-8a0d088e77bf>
2015-04-28 07:38:35 +0000

    Lots of notes and a few fixes. Learning so much! :-)
    git-svn-id: svn://svn.code.sf.net/p/dasm-dillon/code/trunk@394 77a3a24b-2c49-0410-90f5-8a0d088e77bf

--- src/gentest.c
+++ src/gentest.c
@@ -11,9 +11,13 @@
  */
 
 /**
+ * Write a fake for the actual addressing mode in a form that DASM will
+ * accept again.
  */
 static void generate_addressing_mode(FILE *tst, int a)
 {
+	/* TODO: the huge switch/print orgy is ugly, I hope to
+	 * come up with a better way of doing this [phf] */
 	switch (a) {
 	case AM_IMP:
 		fprintf(tst, "\t");
@@ -52,7 +56,8 @@ static void generate_addressing_mode(FILE *tst, int a)
 		fprintf(tst, "\t\t; address16,y [%d]\n", a);
 		break;
 	case AM_REL:
-		fprintf(tst, "\t$40");
+		/* [phf] copied Matt's trick of using . for branches */
+		fprintf(tst, "\t.");
 		fprintf(tst, "\t\t; relative8 [%d]\n", a);
 		break;
 	case AM_INDBYTEX:
@@ -67,25 +72,62 @@ static void generate_addressing_mode(FILE *tst, int a)
 		fprintf(tst, "\t($d00d)");
 		fprintf(tst, "\t\t; (address16) [%d]\n", a);
 		break;
+	/*
+	 * [phf] The following two addressing modes seem inspired by the
+	 * 68705; only the ,x mode is available on that machine BTW,
+	 * I believe the ,y mode was invented by Matthew Dillon just
+	 * for symmetry reasons. If you can find it anywhere "for real"
+	 * please let me know. Maybe we can support yet another CPU
+	 * cheaply that way. :-) The syntax is horrid, just ",x" with
+	 * nothing else. But hey, that's what we're stuck with. I wonder
+	 * what the actual Motorola syntax for this was.
+	 */
 	case AM_0X:
-		fprintf(tst, "\t???");
-		fprintf(tst, "\t\t; ???zero,x??? [%d]\n", a);
+		fprintf(tst, "\t,x");
+		fprintf(tst, "\t\t; implied,x (aka address0 aka zero-offset) [%d]\n", a);
 		break;
 	case AM_0Y:
-		fprintf(tst, "\t???");
-		fprintf(tst, "\t\t; ???zero,y??? [%d]\n", a);
+		fprintf(tst, "\t,y");
+		fprintf(tst, "\t\t; implied,y (aka address0 aka zero-offset) [%d]\n", a);
 		break;
+	/*
+	 * These two addressing modes *also* seem to come from the 68705
+	 * and they are fascinating because they have *three* arguments.
+	 * That's something I thought impossible before when I discussed
+	 * the broken HD6303 opcodes which would also need three. I need
+	 * to investigate these *very* closely.
+	 *
+	 * Alright, these are really weird. So there's a bit number that
+	 * we test for which is encoded into the actual opcode. (I don't
+	 * even know if DASM does that!) Then there's a "direct" address
+	 * which is the 68705's way of saying "zero page" I guess. That's
+	 * all for bclr and bset. For brclr and brset there's a *third*
+	 * byte that specifies the branch offset. So as per usual there's
+	 * a difference between the signed branch offset and the actual
+	 * branch range; the offset is -128 to 127, the branch offset is
+	 * -125 to 130; because the instruction is *three* bytes long as
+	 * opposed to a 6502 branch that's only two bytes long. Again I
+	 * wonder if DASM actually deals with this. Let's look.
+	 *
+	 * Amazing! There are special cases for these in ops.c/v_mnemonic()
+	 * but there are also new MF_ flags to mark the instructions as
+	 * special. Looking into this showed that mne6811.c *also* have
+	 * these kinds of instructions but with different MF_ flags. It's
+	 * fascinating! I might have to add more cases below to properly
+	 * generate the fake addressing mode string. [phf]
+	 */
 	case AM_BITMOD:
-		fprintf(tst, "\t???");
+		fprintf(tst, "\t1,23");
 		fprintf(tst, "\t\t; ???bitmod??? [%d]\n", a);
 		break;
 	case AM_BITBRAMOD:
-		fprintf(tst, "\t???");
+		/* [phf] copied Matt's trick of using . for branches */
+		fprintf(tst, "\t1,23,.");
 		fprintf(tst, "\t\t; ???bitbramod??? [%d]\n", a);
 		break;
 	default:
 		fprintf(tst, "\t???");
-		fprintf(tst, "\t\t; OUT OF RANGE? [%d]\n", a);
+		fprintf(tst, "\t\t; ???invalid addressing mode??? [%d]\n", a);
 		break;
 	}
 }
